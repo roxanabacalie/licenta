ce3eef3ec139310d9d4bd27419f067d663b61cee
roxanabacalie <97856854+roxanabacalie@users.noreply.github.com>
2024-05-30 19:22:42 +0300

    creare folder frontend

--- pythonProject/static/script.js
+++ pythonProject/static/script.js
@@ -1,6 +1,6 @@
 let map;
 let directionsService;
-
+/*
 async function initMap() {
   console.log("map");
   const position = { lat: 47.19052, lng: 27.55848 };
@@ -15,51 +15,6 @@ async function initMap() {
   drawRoutesFromCSV();
   //drawRoutesFromJSON();
 }
-/*
-async function drawRoutesFromJSON() {
-  try {
-    const response = await fetch('../static/directions_data_list.json');
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-
-    data.forEach(routeData => {
-      console.log(routeData)
-      if (routeData.status === 'OK' && routeData.routes.length > 0) {
-        const route = routeData.routes[0]; // Select the first route (if it exists)
-        console.log(routeData.routes[0])
-        const renderer = new google.maps.DirectionsRenderer({
-          polylineOptions: {
-            strokeColor: getRandomColor(),
-            strokeWeight: 5,
-            strokeOpacity: 0.7
-          },
-          suppressMarkers: true
-        });
-        renderer.setMap(map);
-
-        const directions = {
-          routes: [{
-            bounds: route.bounds,
-            legs: route.legs,
-            overview_polyline: route.overview_polyline
-          }],
-          status: 'OK',
-          request: route.request
-        };
-
-        renderer.setDirections(directions);
-      } else {
-        console.error('Could not load directions data: ', routeData.status);
-      }
-    });
-  } catch (error) {
-    console.error('Error fetching directions data: ', error);
-  }
-}
-
-*/
 async function drawRoutesFromCSV() {
   const response = await fetch('../static/stops_data.csv');
   const data = await response.text();
@@ -74,20 +29,19 @@ async function drawRoutesFromCSV() {
   }
 }
 
+
 function saveDirectionsToFile(response) {
-  var jsonData = JSON.stringify(response);
-  var blob = new Blob([jsonData], { type: 'application/json' });
-  var filename = 'directions.json';
-  if (window.navigator.msSaveOrOpenBlob) {
-    window.navigator.msSaveBlob(blob, filename);
-  } else {
-    var elem = window.document.createElement('a');
-    elem.href = window.URL.createObjectURL(blob);
-    elem.download = filename;
-    document.body.appendChild(elem);
-    elem.click();
-    document.body.removeChild(elem);
-  }
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://localhost:8000/directions", true);
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      console.log("Data saved successfully");
+    } else if (xhr.readyState === 4) {
+      console.error("Error saving data");
+    }
+  };
+  xhr.send(JSON.stringify(response));
 }
 
 function drawRoute(start, end, color) {
@@ -148,3 +102,51 @@ function getRandomColor() {
 
 // Ensure initMap is defined in the global scope
 window.initMap = initMap;
+
+*/
+
+/*
+async function drawRoutesFromJSON() {
+  try {
+    const response = await fetch('../static/directions_data_list.json');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+
+    data.forEach(routeData => {
+      console.log(routeData)
+      if (routeData.status === 'OK' && routeData.routes.length > 0) {
+        const route = routeData.routes[0]; // Select the first route (if it exists)
+        console.log(routeData.routes[0])
+        const renderer = new google.maps.DirectionsRenderer({
+          polylineOptions: {
+            strokeColor: getRandomColor(),
+            strokeWeight: 5,
+            strokeOpacity: 0.7
+          },
+          suppressMarkers: true
+        });
+        renderer.setMap(map);
+
+        const directions = {
+          routes: [{
+            bounds: route.bounds,
+            legs: route.legs,
+            overview_polyline: route.overview_polyline
+          }],
+          status: 'OK',
+          request: route.request
+        };
+
+        renderer.setDirections(directions);
+      } else {
+        console.error('Could not load directions data: ', routeData.status);
+      }
+    });
+  } catch (error) {
+    console.error('Error fetching directions data: ', error);
+  }
+}
+
+*/
